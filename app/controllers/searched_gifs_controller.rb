eb18fd3bda9e18808665ff001ee461255399902e
“mpallenjr”git config --global user.email “mpallenjnr@gmail.com” <mikeallen@Michaels-MBP.attlocal.net>
2022-01-23 16:14:18 -0600

    add buffers to current responses.

--- app/controllers/searched_gifs_controller.rb
+++ app/controllers/searched_gifs_controller.rb
@@ -2,7 +2,8 @@ class SearchedGifsController < ApplicationController
   def show
     require "http"
     response = HTTP.get("http://api.giphy.com/v1/gifs/search?q=ryan+gosling&api_key=#{Rails.application.credentials.giphy_api_key}&limit=5")
-    parsedResponse = JSON.parse(response)
+    buffer = response.body
+    parsedResponse = JSON.parse(buffer)
     render json: parsedResponse
   end
 end
--- app/controllers/trending_gifs_controller.rb
+++ app/controllers/trending_gifs_controller.rb
@@ -2,7 +2,8 @@ class TrendingGifsController < ApplicationController
   def index
     require "http"
     response = HTTP.get("https://api.giphy.com/v1/gifs/trending?api_key=#{Rails.application.credentials.giphy_api_key}&limit=25&rating=g")
-    parsedResponse = JSON.parse(response)
+    buffer = response.body
+    parsedResponse = JSON.parse(buffer)
     render json: parsedResponse
   end
 end
